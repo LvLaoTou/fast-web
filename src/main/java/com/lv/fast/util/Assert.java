aa6b405326397f6b7879457e183ccb921ec3825a
jie.lv <jie.lv@saxofintech.com>
2021-07-01 17:12:39 +0800

    feat: 完善Assert断言

--- src/main/java/com/lv/fast/util/Assert.java
+++ src/main/java/com/lv/fast/util/Assert.java
@@ -1,10 +1,10 @@
 package com.lv.fast.util;
 
 import cn.hutool.core.collection.CollectionUtil;
-import com.baomidou.mybatisplus.core.toolkit.StringUtils;
+import com.lv.fast.common.Describe;
 import com.lv.fast.constant.RestResultCodeConstant;
-import com.lv.fast.enums.RestResultEnum;
 import com.lv.fast.exception.MyException;
+import org.apache.commons.lang3.StringUtils;
 
 import java.util.Collection;
 
@@ -18,7 +18,7 @@ public class Assert {
     private Assert(){}
 
     /**
-     * 断言是否不为空，为空则抛出异常
+     * 断言不为空，为空则抛出异常
      * @param target 目标对象
      * @param message 错误描述信息
      */
@@ -27,7 +27,7 @@ public class Assert {
     }
 
     /**
-     * 断言是否不为空，为空则抛出异常
+     * 断言不为空，为空则抛出异常
      * @param target 目标对象
      * @param code 错误状态码
      * @param message 错误描述信息
@@ -39,16 +39,16 @@ public class Assert {
     }
 
     /**
-     * 断言是否不为空，为空则抛出异常
+     * 断言不为空，为空则抛出异常
      * @param target 目标对象
-     * @param restResultEnum 响应枚举对象
+     * @param describe 错误信息
      */
-    public static void assertNotNull(Object target, RestResultEnum restResultEnum){
-        assertNotNull(target, restResultEnum.getCode(), restResultEnum.getDescribe());
+    public static void assertNotNull(Object target, Describe describe){
+        assertNotNull(target, describe.getCode(), describe.getDescribe());
     }
 
     /**
-     * 断言是否不为空，为空则抛出异常
+     * 断言不为空，为空则抛出异常
      * @param target 目标对象
      * @param message 错误描述信息
      */
@@ -57,24 +57,24 @@ public class Assert {
     }
 
     /**
-     * 断言是否不为空，为空则抛出异常
+     * 断言不为空，为空则抛出异常
      * @param target 目标对象
      * @param code 错误状态码
      * @param message 错误描述信息
      */
     public static void assertNotNull(String target, String code, String message){
-        if (StringUtils.isBlank(target)){
+        if (org.apache.commons.lang3.StringUtils.isBlank(target)){
             throw new MyException(code, message);
         }
     }
 
     /**
-     * 断言是否不为空，为空则抛出异常
+     * 断言不为空，为空则抛出异常
      * @param target 目标对象
-     * @param restResultEnum 响应枚举对象
+     * @param describe 错误信息
      */
-    public static void assertNotNull(String target, RestResultEnum restResultEnum){
-        assertNotNull(target, restResultEnum.getCode(), restResultEnum.getDescribe());
+    public static void assertNotNull(String target, Describe describe){
+        assertNotNull(target, describe.getCode(), describe.getDescribe());
     }
 
     /**
@@ -83,24 +83,62 @@ public class Assert {
      * @param message 描述信息
      */
     public static void assertNotNull(Collection<?> target, String message){
+        assertNotNull(target, RestResultCodeConstant.PARAM_ERROR, message);
+    }
+
+    /**
+     * 断言为空  不为空则抛出自定义异常
+     * @param target 目标集合
+     * @param describe 描述信息
+     */
+    public static void assertNotNull(Collection<?> target, Describe describe){
+        assertNotNull(target, describe.getCode(), describe.getDescribe());
+    }
+
+    /**
+     * 断言为空  不为空则抛出自定义异常
+     * @param target 目标集合
+     * @param code 错误码
+     * @param message 描述信息
+     */
+    public static void assertNotNull(Collection<?> target, String code, String message){
         if (CollectionUtil.isEmpty(target)){
-            throw new MyException(RestResultCodeConstant.PARAM_ERROR, message);
+            throw new MyException(code, message);
         }
     }
 
     /**
-     * 断言是否不为空，为空则抛出异常
+     * 断言不为空，为空则抛出异常
      * @param target 目标对象
      * @param message 错误描述信息
      */
     public static void assertNotNull(Object[] target,String message){
+        assertNotNull(target, RestResultCodeConstant.PARAM_ERROR, message);
+    }
+
+    /**
+     * 断言不为空，为空则抛出异常
+     * @param target 目标对象
+     * @param describe 错误描述信息
+     */
+    public static void assertNotNull(Object[] target,Describe describe){
+        assertNotNull(target, describe.getCode(), describe.getDescribe());
+    }
+
+    /**
+     * 断言不为空，为空则抛出异常
+     * @param target 目标对象
+     * @param code 错误码
+     * @param message 错误描述信息
+     */
+    public static void assertNotNull(Object[] target, String code, String message){
         if (target == null || target.length == 0){
-            throw new MyException(RestResultCodeConstant.PARAM_ERROR, message);
+            throw new MyException(code, message);
         }
     }
 
     /**
-     * 断言是否正确
+     * 断言为true  如果为false 抛出自定义异常
      * @param flag 条件
      * @param message 错误描述
      */
@@ -109,21 +147,169 @@ public class Assert {
     }
 
     /**
-     * 断言是否正确
+     * 断言为true  如果为false 抛出自定义异常
      * @param flag 条件
      * @param message 错误描述
      */
     public static void assertIsTrue(boolean flag, String code, String message){
+        if (!flag){
+            throw new MyException(code, message);
+        }
+    }
+    /**
+     * 断言为true  如果为false 抛出自定义异常
+     * @param flag 条件
+     * @param describe 错误信息
+     */
+    public static void assertIsTrue(boolean flag, Describe describe){
+        assertIsTrue(flag, describe.getCode(), describe.getDescribe());
+    }
+
+    /**
+     * 断言为false  如果为true 抛出自定义异常
+     * @param flag 条件
+     * @param message 错误描述
+     */
+    public static void assertIsFalse(boolean flag,String message){
+        assertIsFalse(flag, RestResultCodeConstant.PARAM_ERROR, message);
+    }
+
+    /**
+     * 断言为false  如果为true 抛出自定义异常
+     * @param flag 条件
+     * @param message 错误描述
+     */
+    public static void assertIsFalse(boolean flag, String code, String message){
         if (flag){
             throw new MyException(code, message);
         }
     }
     /**
-     * 断言是否正确
+     * 断言为false  如果为true 抛出自定义异常
      * @param flag 条件
-     * @param restResultEnum 响应枚举对象
+     * @param describe 错误信息
      */
-    public static void assertIsTrue(boolean flag, RestResultEnum restResultEnum){
-        assertIsTrue(flag, restResultEnum.getCode(), restResultEnum.getDescribe());
+    public static void assertIsFalse(boolean flag, Describe describe){
+        assertIsFalse(flag, describe.getCode(), describe.getDescribe());
+    }
+
+    /**
+     * 断言为空，不为空则抛出异常
+     * @param target 目标对象
+     * @param code 错误状态码
+     * @param message 错误描述信息
+     */
+    public static void assertIsNull(String target, String code, String message){
+        if (StringUtils.isNoneBlank(target)){
+            throw new MyException(code, message);
+        }
+    }
+
+    /**
+     * 断言为空，不为空则抛出异常
+     * @param target 目标对象
+     * @param describe 错误信息
+     */
+    public static void assertIsNull(String target, Describe describe){
+        assertIsNull(target, describe.getCode(), describe.getDescribe());
+    }
+
+    /**
+     * 断言为空，不为空则抛出异常
+     * @param target
+     * @param message
+     */
+    public static void assertIsNull(String target,String message){
+        assertIsNull(target, RestResultCodeConstant.PARAM_ERROR, message);
+    }
+
+    /**
+     * 断言为空  不为空则抛出自定义异常
+     * @param target 目标集合
+     * @param message 描述信息
+     */
+    public static void assertIsNull(Collection<?> target, String message){
+        assertIsNull(target, RestResultCodeConstant.PARAM_ERROR, message);
+    }
+
+    /**
+     * 断言为空  不为空则抛出自定义异常
+     * @param target 目标集合
+     * @param describe 描述信息
+     */
+    public static void assertIsNull(Collection<?> target, Describe describe){
+        assertIsNull(target, describe.getCode(), describe.getDescribe());
+    }
+
+    /**
+     * 断言为空  不为空则抛出自定义异常
+     * @param target 目标集合
+     * @param code 错误码
+     * @param message 描述信息
+     */
+    public static void assertIsNull(Collection<?> target, String code, String message){
+        if (CollectionUtil.isNotEmpty(target)){
+            throw new MyException(code, message);
+        }
+    }
+
+    /**
+     * 断言为空，不为空则抛出异常
+     * @param target 目标对象
+     * @param message 错误描述信息
+     */
+    public static void assertIsNull(Object[] target,String message){
+        assertIsNull(target, RestResultCodeConstant.PARAM_ERROR, message);
+    }
+
+    /**
+     * 断言为空，不为空则抛出异常
+     * @param target 目标对象
+     * @param describe 错误描述信息
+     */
+    public static void assertIsNull(Object[] target,Describe describe){
+        assertIsNull(target, describe.getCode(), describe.getDescribe());
+    }
+
+    /**
+     * 断言为空，不为空则抛出异常
+     * @param target 目标对象
+     * @param code 错误码
+     * @param message 错误描述信息
+     */
+    public static void assertIsNull(Object[] target, String code, String message){
+        if (target != null && target.length != 0){
+            throw new MyException(code, message);
+        }
+    }
+
+    /**
+     * 断言为空，不为空则抛出异常
+     * @param target 目标对象
+     * @param message 错误描述信息
+     */
+    public static void assertIsNull(Object target,String message){
+        assertIsNull(target, RestResultCodeConstant.PARAM_ERROR, message);
+    }
+
+    /**
+     * 断言为空，不为空则抛出异常
+     * @param target 目标对象
+     * @param code 错误状态码
+     * @param message 错误描述信息
+     */
+    public static void assertIsNull(Object target, String code, String message){
+        if (target != null){
+            throw new MyException(code, message);
+        }
+    }
+
+    /**
+     * 断言为空，不为空则抛出异常
+     * @param target 目标对象
+     * @param describe 错误信息
+     */
+    public static void assertIsNull(Object target, Describe describe){
+        assertIsNull(target, describe.getCode(), describe.getDescribe());
     }
 }
