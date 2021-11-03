3544d3d079cd1217b02a2dff07e74add5cf059bb
lvlaotou <CoderLvJie@foxmail.com>
2021-11-03 13:53:01 +0800

    feat: 断言工具类增加输出数据到日志的功能

--- src/main/java/com/lv/fast/common/util/Assert.java
+++ src/main/java/com/lv/fast/common/util/Assert.java
@@ -1,9 +1,12 @@
 package com.lv.fast.common.util;
 
 import cn.hutool.core.collection.CollectionUtil;
+import com.lv.fast.common.constant.JsonConstant;
 import com.lv.fast.common.valid.Describe;
 import com.lv.fast.common.constant.RestResultCodeConstant;
 import com.lv.fast.exception.MyException;
+import lombok.SneakyThrows;
+import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.StringUtils;
 
 import java.util.Collection;
@@ -12,9 +15,12 @@ import java.util.Collection;
  * 自定义断言
  * @author lvlaotou
  */
+@Slf4j
 public class Assert {
 
     private Assert(){}
+
+    /******************************************************************Assert Object Not Null***********************************************************************/
 
     /**
      * 断言不为空，为空则抛出异常
@@ -28,13 +34,21 @@ public class Assert {
     /**
      * 断言不为空，为空则抛出异常
      * @param target 目标对象
+     * @param message 错误描述信息
+     * @param logOutData 需要日志打印的数据
+     */
+    public static void assertNotNull(Object target,String message, Object logOutData){
+        assertNotNull(target, RestResultCodeConstant.PARAM_ERROR, message, logOutData);
+    }
+
+    /**
+     * 断言不为空，为空则抛出异常
+     * @param target 目标对象
      * @param code 错误状态码
      * @param message 错误描述信息
      */
     public static void assertNotNull(Object target, String code, String message){
-        if (target == null){
-            throw new MyException(code, message);
-        }
+        assertNotNull(target, code, message, null);
     }
 
     /**
@@ -49,6 +63,33 @@ public class Assert {
     /**
      * 断言不为空，为空则抛出异常
      * @param target 目标对象
+     * @param describe 错误信息
+     * @param logOutData 需要日志打印的数据
+     */
+    public static void assertNotNull(Object target, Describe describe, Object logOutData){
+        assertNotNull(target, describe.getCode().toString(), describe.getDescribe(), logOutData);
+    }
+
+
+    /**
+     * 断言不为空，为空则抛出异常
+     * @param target 目标对象
+     * @param code 错误状态码
+     * @param message 错误描述信息
+     * @param logOutData 需要日志打印的数据
+     */
+    public static void assertNotNull(Object target, String code, String message, Object logOutData){
+        if (target == null){
+            logOutError(logOutData);
+            throw new MyException(code, message);
+        }
+    }
+
+    /******************************************************************Assert String Not Null***********************************************************************/
+
+    /**
+     * 断言不为空，为空则抛出异常
+     * @param target 目标对象
      * @param message 错误描述信息
      */
     public static void assertNotNull(String target,String message){
@@ -58,13 +99,11 @@ public class Assert {
     /**
      * 断言不为空，为空则抛出异常
      * @param target 目标对象
-     * @param code 错误状态码
      * @param message 错误描述信息
+     * @param logOutData 需要日志打印的数据
      */
-    public static void assertNotNull(String target, String code, String message){
-        if (org.apache.commons.lang3.StringUtils.isBlank(target)){
-            throw new MyException(code, message);
-        }
+    public static void assertNotNull(String target,String message, Object logOutData){
+        assertNotNull(target, RestResultCodeConstant.PARAM_ERROR, message, logOutData);
     }
 
     /**
@@ -77,12 +116,59 @@ public class Assert {
     }
 
     /**
+     * 断言不为空，为空则抛出异常
+     * @param target 目标对象
+     * @param describe 错误信息
+     * @param logOutData 需要日志打印的数据
+     */
+    public static void assertNotNull(String target, Describe describe, Object logOutData){
+        assertNotNull(target, describe.getCode().toString().toString(), describe.getDescribe(), logOutData);
+    }
+
+
+    /**
+     * 断言不为空，为空则抛出异常
+     * @param target 目标对象
+     * @param code 错误状态码
+     * @param message 错误描述信息
+     */
+    public static void assertNotNull(String target, String code, String message){
+        assertNotNull(target, code, message, null);
+    }
+
+    /**
+     * 断言不为空，为空则抛出异常
+     * @param target 目标对象
+     * @param code 错误状态码
+     * @param message 错误描述信息
+     * @param logOutData 需要日志打印的数据
+     */
+    public static void assertNotNull(String target, String code, String message, Object logOutData){
+        if (StringUtils.isBlank(target)){
+            logOutError(logOutData);
+            throw new MyException(code, message);
+        }
+    }
+
+    /******************************************************************Assert Collection Not Null***********************************************************************/
+
+    /**
      * 断言不为空  为空则抛出自定义异常
      * @param target 目标集合
      * @param message 描述信息
      */
     public static void assertNotNull(Collection<?> target, String message){
         assertNotNull(target, RestResultCodeConstant.PARAM_ERROR, message);
+    }
+
+    /**
+     * 断言不为空  为空则抛出自定义异常
+     * @param target 目标集合
+     * @param message 描述信息
+     * @param logOutData 需要日志打印的数据
+     */
+    public static void assertNotNull(Collection<?> target, String message, Object logOutData){
+        assertNotNull(target, RestResultCodeConstant.PARAM_ERROR, message, logOutData);
     }
 
     /**
@@ -97,14 +183,38 @@ public class Assert {
     /**
      * 断言为空  不为空则抛出自定义异常
      * @param target 目标集合
+     * @param describe 描述信息
+     * @param logOutData 需要日志打印的数据
+     */
+    public static void assertNotNull(Collection<?> target, Describe describe, Object logOutData){
+        assertNotNull(target, describe.getCode().toString(), describe.getDescribe(), logOutData);
+    }
+
+    /**
+     * 断言为空  不为空则抛出自定义异常
+     * @param target 目标集合
      * @param code 错误码
      * @param message 描述信息
      */
     public static void assertNotNull(Collection<?> target, String code, String message){
+        assertNotNull(target, code, message, null);
+    }
+
+    /**
+     * 断言为空  不为空则抛出自定义异常
+     * @param target 目标集合
+     * @param code 错误码
+     * @param message 描述信息
+     * @param logOutData 需要日志打印的数据
+     */
+    public static void assertNotNull(Collection<?> target, String code, String message, Object logOutData){
         if (CollectionUtil.isEmpty(target)){
+            logOutError(logOutData);
             throw new MyException(code, message);
         }
     }
+
+    /******************************************************************Assert Array Not Null***********************************************************************/
 
     /**
      * 断言不为空，为空则抛出异常
@@ -113,6 +223,16 @@ public class Assert {
      */
     public static void assertNotNull(Object[] target,String message){
         assertNotNull(target, RestResultCodeConstant.PARAM_ERROR, message);
+    }
+
+    /**
+     * 断言不为空，为空则抛出异常
+     * @param target 目标对象
+     * @param message 错误描述信息
+     * @param logOutData 需要日志打印的数据
+     */
+    public static void assertNotNull(Object[] target,String message, Object logOutData){
+        assertNotNull(target, RestResultCodeConstant.PARAM_ERROR, message, logOutData);
     }
 
     /**
@@ -127,14 +247,38 @@ public class Assert {
     /**
      * 断言不为空，为空则抛出异常
      * @param target 目标对象
+     * @param describe 错误描述信息
+     * @param logOutData 需要日志打印的数据
+     */
+    public static void assertNotNull(Object[] target,Describe describe, Object logOutData){
+        assertNotNull(target, describe.getCode().toString(), describe.getDescribe(), logOutData);
+    }
+
+    /**
+     * 断言不为空，为空则抛出异常
+     * @param target 目标对象
      * @param code 错误码
      * @param message 错误描述信息
      */
     public static void assertNotNull(Object[] target, String code, String message){
+        assertNotNull(target, code, message, null);
+    }
+
+    /**
+     * 断言不为空，为空则抛出异常
+     * @param target 目标对象
+     * @param code 错误码
+     * @param message 错误描述信息
+     * @param logOutData 需要日志打印的数据
+     */
+    public static void assertNotNull(Object[] target, String code, String message, Object logOutData){
         if (target == null || target.length == 0){
+            logOutError(logOutData);
             throw new MyException(code, message);
         }
     }
+
+    /******************************************************************Assert Boolean Is True***********************************************************************/
 
     /**
      * 断言为true  如果为false 抛出自定义异常
@@ -149,12 +293,12 @@ public class Assert {
      * 断言为true  如果为false 抛出自定义异常
      * @param flag 条件
      * @param message 错误描述
+     * @param logOutData 需要日志打印的数据
      */
-    public static void assertIsTrue(boolean flag, String code, String message){
-        if (!flag){
-            throw new MyException(code, message);
-        }
+    public static void assertIsTrue(boolean flag, String message, Object logOutData){
+        assertIsTrue(flag, RestResultCodeConstant.PARAM_ERROR, message, logOutData);
     }
+
     /**
      * 断言为true  如果为false 抛出自定义异常
      * @param flag 条件
@@ -163,6 +307,40 @@ public class Assert {
     public static void assertIsTrue(boolean flag, Describe describe){
         assertIsTrue(flag, describe.getCode().toString(), describe.getDescribe());
     }
+
+    /**
+     * 断言为true  如果为false 抛出自定义异常
+     * @param flag 条件
+     * @param describe 错误信息
+     * @param logOutData 需要日志打印的数据
+     */
+    public static void assertIsTrue(boolean flag, Describe describe, Object logOutData){
+        assertIsTrue(flag, describe.getCode().toString(), describe.getDescribe(), logOutData);
+    }
+
+    /**
+     * 断言为true  如果为false 抛出自定义异常
+     * @param flag 条件
+     * @param message 错误描述
+     */
+    public static void assertIsTrue(boolean flag, String code, String message){
+        assertIsTrue(flag, code, message, null);
+    }
+
+    /**
+     * 断言为true  如果为false 抛出自定义异常
+     * @param flag 条件
+     * @param message 错误描述
+     * @param logOutData 需要日志打印的数据
+     */
+    public static void assertIsTrue(boolean flag, String code, String message, Object logOutData){
+        if (!flag){
+            logOutError(logOutData);
+            throw new MyException(code, message);
+        }
+    }
+
+    /******************************************************************Assert Boolean Is False***********************************************************************/
 
     /**
      * 断言为false  如果为true 抛出自定义异常
@@ -177,12 +355,12 @@ public class Assert {
      * 断言为false  如果为true 抛出自定义异常
      * @param flag 条件
      * @param message 错误描述
+     * @param logOutData 需要日志打印的数据
      */
-    public static void assertIsFalse(boolean flag, String code, String message){
-        if (flag){
-            throw new MyException(code, message);
-        }
+    public static void assertIsFalse(boolean flag,String message, Object logOutData){
+        assertIsFalse(flag, RestResultCodeConstant.PARAM_ERROR, message, logOutData);
     }
+
     /**
      * 断言为false  如果为true 抛出自定义异常
      * @param flag 条件
@@ -193,16 +371,39 @@ public class Assert {
     }
 
     /**
-     * 断言为空，不为空则抛出异常
-     * @param target 目标对象
-     * @param code 错误状态码
-     * @param message 错误描述信息
+     * 断言为false  如果为true 抛出自定义异常
+     * @param flag 条件
+     * @param describe 错误信息
+     * @param logOutData 需要日志打印的数据
      */
-    public static void assertIsNull(String target, String code, String message){
-        if (StringUtils.isNoneBlank(target)){
+    public static void assertIsFalse(boolean flag, Describe describe, Object logOutData){
+        assertIsFalse(flag, describe.getCode().toString(), describe.getDescribe(), logOutData);
+    }
+
+    /**
+     * 断言为false  如果为true 抛出自定义异常
+     * @param flag 条件
+     * @param message 错误描述
+     */
+    public static void assertIsFalse(boolean flag, String code, String message){
+        assertIsFalse(flag, code, message, null);
+    }
+
+    /**
+     * 断言为false  如果为true 抛出自定义异常
+     * @param flag 条件
+     * @param message 错误描述
+     * @param logOutData 需要日志打印的数据
+     */
+    public static void assertIsFalse(boolean flag, String code, String message, Object logOutData){
+        if (flag){
+            logOutError(logOutData);
             throw new MyException(code, message);
         }
     }
+
+    /******************************************************************Assert String Is Null***********************************************************************/
+
 
     /**
      * 断言为空，不为空则抛出异常
@@ -215,12 +416,58 @@ public class Assert {
 
     /**
      * 断言为空，不为空则抛出异常
-     * @param target
-     * @param message
+     * @param target 目标对象
+     * @param describe 错误信息
+     * @param logOutData 需要日志打印的数据
+     */
+    public static void assertIsNull(String target, Describe describe, Object logOutData){
+        assertIsNull(target, describe.getCode().toString(), describe.getDescribe(), logOutData);
+    }
+
+    /**
+     * 断言为空，不为空则抛出异常
+     * @param target 目标对象
+     * @param message 错误描述
      */
     public static void assertIsNull(String target,String message){
         assertIsNull(target, RestResultCodeConstant.PARAM_ERROR, message);
     }
+
+    /**
+     * 断言为空，不为空则抛出异常
+     * @param target 目标对象
+     * @param message 错误描述
+     * @param logOutData 需要日志打印的数据
+     */
+    public static void assertIsNull(String target,String message, Object logOutData){
+        assertIsNull(target, RestResultCodeConstant.PARAM_ERROR, message, logOutData);
+    }
+
+    /**
+     * 断言为空，不为空则抛出异常
+     * @param target 目标对象
+     * @param code 错误状态码
+     * @param message 错误描述信息
+     */
+    public static void assertIsNull(String target, String code, String message){
+        assertIsNull(target, code, message, null);
+    }
+
+    /**
+     * 断言为空，不为空则抛出异常
+     * @param target 目标对象
+     * @param code 错误状态码
+     * @param message 错误描述信息
+     * @param logOutData 需要日志打印的数据
+     */
+    public static void assertIsNull(String target, String code, String message, Object logOutData){
+        if (StringUtils.isNoneBlank(target)){
+            logOutError(logOutData);
+            throw new MyException(code, message);
+        }
+    }
+
+    /******************************************************************Assert Collection Is Null***********************************************************************/
 
     /**
      * 断言为空  不为空则抛出自定义异常
@@ -229,6 +476,16 @@ public class Assert {
      */
     public static void assertIsNull(Collection<?> target, String message){
         assertIsNull(target, RestResultCodeConstant.PARAM_ERROR, message);
+    }
+
+    /**
+     * 断言为空  不为空则抛出自定义异常
+     * @param target 目标集合
+     * @param message 描述信息
+     * @param logOutData 需要日志打印的数据
+     */
+    public static void assertIsNull(Collection<?> target, String message, Object logOutData){
+        assertIsNull(target, RestResultCodeConstant.PARAM_ERROR, message, logOutData);
     }
 
     /**
@@ -243,22 +500,56 @@ public class Assert {
     /**
      * 断言为空  不为空则抛出自定义异常
      * @param target 目标集合
+     * @param describe 描述信息
+     * @param logOutData 需要日志打印的数据
+     */
+    public static void assertIsNull(Collection<?> target, Describe describe, Object logOutData){
+        assertIsNull(target, describe.getCode().toString(), describe.getDescribe(), logOutData);
+    }
+
+    /**
+     * 断言为空  不为空则抛出自定义异常
+     * @param target 目标集合
      * @param code 错误码
      * @param message 描述信息
      */
     public static void assertIsNull(Collection<?> target, String code, String message){
+        assertIsNull(target, code, message, null);
+    }
+
+    /**
+     * 断言为空  不为空则抛出自定义异常
+     * @param target 目标集合
+     * @param code 错误码
+     * @param message 描述信息
+     * @param logOutData 需要日志打印的数据
+     */
+    public static void assertIsNull(Collection<?> target, String code, String message, Object logOutData){
         if (CollectionUtil.isNotEmpty(target)){
+            logOutError(logOutData);
             throw new MyException(code, message);
         }
     }
+
+    /******************************************************************Assert Array Is Null***********************************************************************/
 
     /**
      * 断言为空，不为空则抛出异常
      * @param target 目标对象
      * @param message 错误描述信息
      */
-    public static void assertIsNull(Object[] target,String message){
+    public static void assertIsNull(Object[] target, String message){
         assertIsNull(target, RestResultCodeConstant.PARAM_ERROR, message);
+    }
+
+    /**
+     * 断言为空，不为空则抛出异常
+     * @param target 目标对象
+     * @param message 错误描述信息
+     * @param logOutData 需要日志打印的数据
+     */
+    public static void assertIsNull(Object[] target, String message, Object logOutData){
+        assertIsNull(target, RestResultCodeConstant.PARAM_ERROR, message, logOutData);
     }
 
     /**
@@ -273,14 +564,38 @@ public class Assert {
     /**
      * 断言为空，不为空则抛出异常
      * @param target 目标对象
+     * @param describe 错误描述信息
+     * @param logOutData 需要日志打印的数据
+     */
+    public static void assertIsNull(Object[] target,Describe describe, Object logOutData){
+        assertIsNull(target, describe.getCode().toString(), describe.getDescribe(), logOutData);
+    }
+
+    /**
+     * 断言为空，不为空则抛出异常
+     * @param target 目标对象
      * @param code 错误码
      * @param message 错误描述信息
      */
     public static void assertIsNull(Object[] target, String code, String message){
+        assertIsNull(target, code, message, null);
+    }
+
+    /**
+     * 断言为空，不为空则抛出异常
+     * @param target 目标对象
+     * @param code 错误码
+     * @param message 错误描述信息
+     * @param logOutData 需要日志打印的数据
+     */
+    public static void assertIsNull(Object[] target, String code, String message, Object logOutData){
         if (target != null && target.length != 0){
+            logOutError(logOutData);
             throw new MyException(code, message);
         }
     }
+
+    /******************************************************************Assert Object Is Null***********************************************************************/
 
     /**
      * 断言为空，不为空则抛出异常
@@ -294,13 +609,11 @@ public class Assert {
     /**
      * 断言为空，不为空则抛出异常
      * @param target 目标对象
-     * @param code 错误状态码
      * @param message 错误描述信息
+     * @param logOutData 需要日志打印的数据
      */
-    public static void assertIsNull(Object target, String code, String message){
-        if (target != null){
-            throw new MyException(code, message);
-        }
+    public static void assertIsNull(Object target,String message, Object logOutData){
+        assertIsNull(target, RestResultCodeConstant.PARAM_ERROR, message, logOutData);
     }
 
     /**
@@ -310,5 +623,52 @@ public class Assert {
      */
     public static void assertIsNull(Object target, Describe describe){
         assertIsNull(target, describe.getCode().toString(), describe.getDescribe());
+    }
+
+    /**
+     * 断言为空，不为空则抛出异常
+     * @param target 目标对象
+     * @param describe 错误信息
+     * @param logOutData 需要日志打印的数据
+     */
+    public static void assertIsNull(Object target, Describe describe, Object logOutData){
+        assertIsNull(target, describe.getCode().toString(), describe.getDescribe(), logOutData);
+    }
+
+    /**
+     * 断言为空，不为空则抛出异常
+     * @param target 目标对象
+     * @param code 错误状态码
+     * @param message 错误描述信息
+     */
+    public static void assertIsNull(Object target, String code, String message){
+        assertIsNull(target, code, message, null);
+    }
+
+    /**
+     * 断言为空，不为空则抛出异常
+     * @param target 目标对象
+     * @param code 错误状态码
+     * @param message 错误描述信息
+     * @param logOutData 需要日志打印的数据
+     */
+    public static void assertIsNull(Object target, String code, String message, Object logOutData){
+        if (target != null){
+            logOutError(logOutData);
+            throw new MyException(code, message);
+        }
+    }
+
+    /******************************************************************Log Out***********************************************************************/
+
+    /**
+     * 输出错误日志
+     * @param logOutData 日志输出数据
+     */
+    @SneakyThrows
+    public static void logOutError(Object logOutData){
+        if (logOutData != null){
+            log.error("断言不为空日志数据：", JsonConstant.MAPPER.writeValueAsString(logOutData));
+        }
     }
 }
