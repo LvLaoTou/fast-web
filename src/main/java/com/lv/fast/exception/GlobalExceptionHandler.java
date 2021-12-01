64d4e54c86ee37cd7423d950c5843bdd34ce6719
jie.lv <jie.lv@saxofintech.com>
2021-12-01 10:12:27 +0800

    refactor: 重构全局异常拦截处理

--- src/main/java/com/lv/fast/exception/GlobalExceptionHandler.java
+++ src/main/java/com/lv/fast/exception/GlobalExceptionHandler.java
@@ -22,103 +22,74 @@ import java.util.concurrent.ExecutionException;
 @Slf4j
 public class GlobalExceptionHandler {
 
-    /**
-     * 拦截自定义异常
-     * @param e 异常信息
-     * @return R对象
-     */
     @ExceptionHandler(MyException.class)
     public RestResult handle(MyException e) {
         log.error("发生自定义异常", e);
         return RestResult.build(e);
     }
 
-    /**
-     * 拦截多线程异常
-     * @param e 多线程异常
-     * @return
-     */
     @ExceptionHandler(UndeclaredThrowableException.class)
     public RestResult handle(UndeclaredThrowableException e) {
-        Throwable cause = e.getCause();
-        if (cause instanceof ExecutionException){
-            return handle((ExecutionException) cause);
-        }
-        log.error("多线程异常", e);
-        return RestResult.error();
+        return handle(e.getCause());
     }
 
-    /**
-     * 拦截多线程异常
-     * @param e 多线程异常
-     * @return
-     */
     @ExceptionHandler(ExecutionException.class)
     public RestResult handle(ExecutionException e) {
-        Throwable cause = e.getCause();
-        if (cause instanceof MyException){
-            return handle((MyException) cause);
-        }
-        if (cause instanceof  BindException){
-            return handle((BindException) cause);
-        }
-        if (cause instanceof  DuplicateKeyException){
-            return handle((DuplicateKeyException) cause);
-        }
-        if (cause instanceof  NoHandlerFoundException){
-            return handle((NoHandlerFoundException) cause);
-        }
-        log.error("多线程异常", e);
-        return RestResult.error();
+        return handle(e.getCause());
     }
 
-    /**
-     * 拦截参数校验异常
-     * @param e 异常信息
-     * @return R对象
-     */
     @ExceptionHandler(BindException.class)
     public RestResult handle(BindException e) {
         log.error("请求参数异常", e);
         String message = "参数错误";
         FieldError fieldError = e.getBindingResult().getFieldError();
         if (fieldError != null){
-            message += ":"+fieldError.getDefaultMessage();
+            String field = fieldError.getField();
+            message += ",参数名:"+field;
+            String defaultMessage = fieldError.getDefaultMessage();
+            if (defaultMessage.contains("nested exception")){
+                defaultMessage = "类型错误";
+            }
+            message += ",错误描述:"+defaultMessage;
         }
         return RestResult.build(RestResultEnum.PARAM_ERROR).withMessage(message);
     }
 
-    /**
-     * 数据库记录已存在错误
-     * @param e 异常对象
-     * @return R对象
-     */
     @ExceptionHandler(DuplicateKeyException.class)
     public RestResult handle(DuplicateKeyException e){
         log.error("数据库异常", e);
         return RestResult.build(RestResultEnum.DATABASE_EXIST_ERROR);
     }
 
-
-    /**
-     * 拦截路径错误
-     * @param e 异常信息
-     * @return R对象
-     */
     @ExceptionHandler(NoHandlerFoundException.class)
     public RestResult handle(NoHandlerFoundException e) {
         log.error("路径异常，没有对应的处理器", e);
         return RestResult.build(RestResultEnum.PATH_NOT_FOUND);
     }
 
-    /**
-     * 拦截所有异常
-     * @param e 异常信息
-     * @return R对象
-     */
     @ExceptionHandler(Exception.class)
     public RestResult handle(Exception e){
         log.error("未知异常", e);
+        return RestResult.error();
+    }
+
+    public RestResult handle(Throwable throwable){
+        if (throwable instanceof ExecutionException){
+            return handle((ExecutionException) throwable);
+        }
+        if (throwable instanceof UndeclaredThrowableException){
+            return handle((UndeclaredThrowableException) throwable);
+        }
+        if (throwable instanceof MyException){
+            return handle((MyException) throwable);
+        }
+        if (throwable instanceof BindException){
+            return handle((BindException) throwable);
+        }
+        if (throwable instanceof DuplicateKeyException){
+            return handle((DuplicateKeyException) throwable);
+        }
+        log.error("发生异常", throwable);
         return RestResult.error();
     }
 }
