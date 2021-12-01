96c5cd015f1c2a7560450d2368ab16faf938dca2
jie.lv <jie.lv@saxofintech.com>
2021-12-01 10:04:48 +0800

    refactor: 去掉无用配置

--- src/main/java/com/lv/fast/common/constant/HttpConstant.java
+++ src/main/java/com/lv/fast/common/constant/HttpConstant.java
@@ -12,19 +12,4 @@ public class HttpConstant {
      * 后台管理模块  token请求头key
      */
     public static final String TOKEN_HEADER_KEY_SYS = "token";
-
-    /**
-     * 跨域请求方式
-     */
-    public static final String REQUEST_CORS_METHOD = "options";
-
-    /**
-     * 在request存放错误信息的Attribute的key
-     */
-    public static final String REQUEST_ATTRIBUTE_ERROR_MESSAGE_KEY = "message";
-
-    /**
-     * 在request存放错误码的Attribute的key
-     */
-    public static final String REQUEST_ATTRIBUTE_ERROR_CODE_KEY = "code";
 }
