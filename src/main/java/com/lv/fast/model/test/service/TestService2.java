09acd6e91d803999211bb802c34e666a730659e1
jie.lv <jie.lv@saxofintech.com>
2021-12-03 17:07:26 +0800

    feat: 业务参考模块增加记录业务日志嵌套使用实例

--- src/main/java/com/lv/fast/model/test/service/TestService2.java
+++ src/main/java/com/lv/fast/model/test/service/TestService2.java
@@ -1,30 +1,27 @@
 package com.lv.fast.model.test.service;
 
 import com.lv.fast.common.log.LogRecord;
+import com.lv.fast.common.log.LogRecordConstant;
 import com.lv.fast.common.log.LogRecordContext;
 import com.lv.fast.common.log.OperateTypeEnum;
 import com.lv.fast.model.test.dto.TestRequest;
-import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 /**
  * @author jie.lv
  */
 @Service
-@RequiredArgsConstructor
-public class TestService {
-
-    private final TestService2 testService2;
+public class TestService2 {
 
     @LogRecord(success = "'请求id:'+#request.id+',请求姓名:'+#request.name+'方法名:'+#methodName+'请求名称:'+#requestName",
-            operateType = OperateTypeEnum.UPDATE,
+            operateType = OperateTypeEnum.SELECT,
             bizNo = "T(java.util.UUID).randomUUID()",
-            fail = "'执行获取请求姓名失败'",
+            fail = "'执行嵌套,错误信息:'+#"+ LogRecordConstant.ERROR_MESSAGE_EVALUATION,
             condition = "#request.success")
-    public String convert(TestRequest request){
-        LogRecordContext.putVariable("methodName", "convert");
-        testService2.test(request);
-        LogRecordContext.putVariable("requestName", "convert");
-        return request.getName();
+    public long test(TestRequest request){
+        LogRecordContext.putVariable("methodName", "test");
+        LogRecordContext.putVariable("requestName", "testRequest");
+        int i = 1 / 0;
+        return request.getId();
     }
 }
