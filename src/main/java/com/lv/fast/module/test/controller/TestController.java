f6b1985a5d8a2c472ab12c872e77c0c3f57a8467
LvLaoTou <CoderLvJie@foxmail.com>
2022-06-08 16:02:09 +0800

    feat: 增加redis注解使用示例

--- src/main/java/com/lv/fast/module/test/controller/TestController.java
+++ src/main/java/com/lv/fast/module/test/controller/TestController.java
@@ -7,10 +7,7 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * @author jie.lv
@@ -27,5 +24,22 @@ public class TestController {
     @Operation(summary = "测试post请求", description = "测试post请求")
     public RestResult test(@RequestBody @Validated TestRequest request){
         return RestResult.success(testService.convert(request));
+    }
+
+    @GetMapping("/testHash")
+    public RestResult testHash(@RequestParam long param){
+        return RestResult.success(testService.testRedisCache(param));
+    }
+
+    @GetMapping("/testEvict")
+    public RestResult testEvict(@RequestParam long param){
+        testService.testRedisEvict(param);
+        return RestResult.success();
+    }
+
+    @GetMapping("/testBatchEvict")
+    public RestResult testBatchEvict(@RequestParam long param){
+        testService.testRedisBatchEvict(param);
+        return RestResult.success();
     }
 }
--- src/main/java/com/lv/fast/module/test/service/TestService.java
+++ src/main/java/com/lv/fast/module/test/service/TestService.java
@@ -4,8 +4,13 @@ import com.lv.fast.common.aop.AopContext;
 import com.lv.fast.common.log.LogRecord;
 import com.lv.fast.common.log.OperateTypeEnum;
 import com.lv.fast.module.test.dto.TestRequest;
+import com.lv.fast.redis.RedisBatchEvict;
+import com.lv.fast.redis.RedisEvict;
+import com.lv.fast.redis.RedisHashCache;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.concurrent.TimeUnit;
 
 /**
  * @author jie.lv
@@ -26,5 +31,56 @@ public class TestService {
         testService2.test(request);
         AopContext.putVariable("requestName", "convert");
         return request.getName();
+    }
+
+    @RedisHashCache(
+            key = "T(com.lv.fast.redis.RedisConstant).SYSTEM_KEY_PREFIX+'test:annotation:redisHashCache'",
+            hashKey = "#param",
+            condition = "#param > 10",
+            unless = "#result > 0",
+            timeout = 1,
+            unit = TimeUnit.HOURS
+    )
+    public long testRedisCache(long param){
+        System.out.println("执行业务:"+param);
+        return param + 1;
+    }
+
+    @RedisEvict(
+            key = "T(com.lv.fast.redis.RedisConstant).SYSTEM_KEY_PREFIX+'test:annotation:redisHashCache'",
+            hashKey = "#param",
+            unless = "#param < 20"
+    )
+    public void testRedisEvict(long param){
+        System.out.println("执行清除缓存业务");
+    }
+
+    @RedisBatchEvict(
+            value = {
+                    @RedisEvict(
+                            key = "T(com.lv.fast.redis.RedisConstant).SYSTEM_KEY_PREFIX+'test:annotation:redisBatchEvict'",
+                            unless = "#param < 10"
+                    ),
+                    @RedisEvict(
+                            key = "T(com.lv.fast.redis.RedisConstant).SYSTEM_KEY_PREFIX+'test:annotation:redisEvict'"
+                    ),
+                    @RedisEvict(
+                            key = "T(com.lv.fast.redis.RedisConstant).SYSTEM_KEY_PREFIX+'test:annotation:redisHashCache'",
+                            hashKey = "#param"
+                    ),
+                    @RedisEvict(
+                            key = "T(com.lv.fast.redis.RedisConstant).SYSTEM_KEY_PREFIX+'test:annotation:redisHashCache'",
+                            hashKey = "#param + 1",
+                            unless = "#param == 15"
+                    ),
+                    @RedisEvict(
+                            key = "T(com.lv.fast.redis.RedisConstant).SYSTEM_KEY_PREFIX+'test:annotation:redisHashCache'",
+                            hashKey = "#param - 1"
+                    )
+            },
+            unless = "#param > 10"
+    )
+    public void testRedisBatchEvict(long param){
+        System.out.printf("执行批量清除缓存业务");
     }
 }
