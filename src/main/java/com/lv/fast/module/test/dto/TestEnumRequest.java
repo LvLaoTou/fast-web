e236f559197f3dcb52580a8036b15fdc4abd00de
LvLaoTou <coderlvjie@foxmail.com>
2026-07-25 23:23:50 +0800

    docs(test): 为 TestEnumRequest 补充缺失的类级 Javadoc

--- src/main/java/com/lv/fast/module/test/dto/TestEnumRequest.java
+++ src/main/java/com/lv/fast/module/test/dto/TestEnumRequest.java
@@ -3,6 +3,10 @@ package com.lv.fast.module.test.dto;
 import com.lv.fast.module.test.enums.TestEnum;
 import lombok.Data;
 
+/**
+ * 测试枚举请求参数对象
+ * @author lvlaotou
+ */
 @Data
 public class TestEnumRequest {
 
