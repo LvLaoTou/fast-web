4a7c5d357758ec89de1744abd4ab1a2fde66483a
LvLaoTou <coderlvjie@foxmail.com>
2026-07-25 23:19:45 +0800

    refactor(application): 移除未使用的静态 applicationContext 字段
    该字段从未被读取，属于无用代码，直接删除。

--- src/main/java/com/lv/fast/Application.java
+++ src/main/java/com/lv/fast/Application.java
@@ -3,7 +3,6 @@ package com.lv.fast;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
-import org.springframework.context.ApplicationContext;
 
 /**
  * @author lvlaotou
@@ -12,10 +11,8 @@ import org.springframework.context.ApplicationContext;
 @SpringBootApplication
 public class Application {
 
-    public static ApplicationContext applicationContext;
-
     public static void main(String[] args) {
-        applicationContext = SpringApplication.run(Application.class, args);
+        SpringApplication.run(Application.class, args);
     }
 
 }
